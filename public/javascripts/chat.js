88dd600e042644358a04bb55a15b0736cea70418
Prarish <hsrishika@gmail.com>
2018-04-14 12:12:36 +0000

    4th commit

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,21 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
+    $scope.messages = [{
 
+            'sender': 'User',
+            'text': 'hello'
+},
+        {
+            'sender': 'BOT',
+            'text': 'hey! how r u?'
+},
+        {
+            'sender': 'USER',
+            'text': 'gud how r u?'
+},
+        {
+            'sender': 'BOT',
+            'text': 'fine'
+}
+];
 });
