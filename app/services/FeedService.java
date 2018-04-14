3230e3551cba065ac875db304924953392e31503
Prarish <hsrishika@gmail.com>
2018-04-14 07:29:36 +0000

    second commit

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -11,5 +11,25 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+    public FeedResponse getFeedByQuery(String query) {
+
+
+        FeedResponse feedResponse = new FeedResponse();
+        try {
+            WSRequest feedRequest = WS.url("https://news.google.com/news?q=CCL&output=rss");
+            CompletionStage<WSResponse> responsePromise = feedRequest
+                    .setQueryParameter("q", "query")
+                    .setQueryParameter("output", "rss")
+                    .get();
+            Document response = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            Node item = response.getFirstChild().getFirstChild().getChildNodes().item(10);
+            feedResponse.title = item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            feedResponse.pubDate = item.getChildNodes().item(3).getFirstChild().getNodeValue();
+            feedResponse.description = item.getChildNodes().item(4).getFirstChild().getNodeValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return feedResponse;
+    }
 
 }
